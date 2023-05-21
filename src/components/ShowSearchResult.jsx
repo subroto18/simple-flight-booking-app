90bbda83694c7285c7778a5148f3e2d25ac6d433
subroto18 <subrotochakraborty18@gmail.com>
2023-05-21 10:31:24 +0530

    adding booking button style

--- src/components/ShowSearchResult.jsx
+++ src/components/ShowSearchResult.jsx
@@ -83,24 +83,39 @@ export const ShowSearchResult = () => {
                   alt={item.flight_name}
                   className="h-14 w-full rounded mt-1"
                 />
-                <p className="border-2 bg-slate-300 px-5 py-3 font-bold ">
+                <p className="border-2 bg-slate-300 px-5 py-3 font-bold relative">
                   {item.flight_name}
+                  <span className="text-sm absolute  right-0 top-10 bg-red-500 text-white p-2 rounded z-40">
+                    Flight Name
+                  </span>
                 </p>
-                <p className="border-2 bg-slate-300 px-5 py-3 font-bold ">
+                <p className="border-2 bg-slate-300 px-5 py-3 font-bold relative">
                   {item.departure_time}
+                  <span className="text-sm absolute  right-0 top-10 bg-red-500 text-white p-2 rounded z-40">
+                    Departure time
+                  </span>
                 </p>
-                <p className="border-2 bg-slate-300 px-5 py-3 font-bold">
+                <p className="border-2 bg-slate-300 px-5 py-3 font-bold relative">
                   {item.flight_duration}
+                  <span className="text-sm absolute  right-0 top-10 bg-red-500 text-white p-2 rounded z-40">
+                    Flight Duration
+                  </span>
                 </p>
-                <p className="border-2 bg-slate-300 px-5 py-3 font-bold ">
+                <p className="border-2 bg-slate-300 px-5 py-3 font-bold relative">
                   {item.reaching_time}
+                  <span className="text-sm absolute  right-0 top-10 bg-red-500 text-white p-2 rounded z-40">
+                    Reaching time
+                  </span>
                 </p>
-                <p className="border-2 bg-slate-300 px-5 py-3 font-bold">
+                <p className="border-2 bg-slate-300 px-5 py-3 font-bold relative">
                   ₹ {item.flight_fare}
+                  <span className="text-sm absolute  right-0 top-10 bg-red-500 text-white p-2 rounded z-40">
+                    Flight Fare
+                  </span>
                 </p>
                 <button
                   onClick={() => onBookFlight(item)}
-                  className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold  px-10  py-3 w-full rounded"
+                  className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold  px-10  py-3 w-full rounded mt-7 lg:mt-0 "
                 >
                   Book Now
                 </button>

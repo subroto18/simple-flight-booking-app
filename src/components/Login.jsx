ba1ef4efed81a0fe6418b887f1daaeddd49b250b
subroto18 <subrotochakraborty18@gmail.com>
2023-05-21 16:30:31 +0530

    backend link added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -66,10 +66,18 @@ export const Login = () => {
         <div className="flex items-center justify-between">
           <button
             onClick={login}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
             type="button"
           >
             Sign In
+          </button>
+
+          <button
+            onClick={() => navigate("/dashboard")}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Redirect to Dashboard
           </button>
         </div>
       </form>
